b3b21201d112811371f25087544b1cad595586fc
denarced <denarced@gmail.com>
2017-11-25 21:38:09 +0200

    Use mocha's --watch option instead of bash/inotifywait to test continuously.

--- test.sh
+++ test.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-while :
-do
-    clear
-    echo -n "Run tests @ "
-    date -Iseconds
-    ./test_once.sh
+watch=""
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -h, --help        Show this output"
+    echo "  -w, --watch       Watch files for change"
+    exit 1
+elif [ "$1" == "-w" ] || [ "$1" == "--watch" ]
+then
+    watch="-w"
+fi
 
-    inotifywait --quiet `find src/ test/ -type f -name \*.js`
-done
+nyc=./node_modules/.bin/nyc
+mocha=./node_modules/.bin/mocha
+crossenv=./node_modules/.bin/cross-env
+
+$crossenv NODE_ENV=test \
+    $nyc $mocha \
+        --require babel-core/register \
+        $watch
--- test_once.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nyc=./node_modules/.bin/nyc
-mocha=./node_modules/.bin/mocha
-crossenv=./node_modules/.bin/cross-env
-
-$crossenv NODE_ENV=test \
-    $nyc $mocha --require babel-core/register
